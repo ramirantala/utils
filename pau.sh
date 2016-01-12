a69e8a781a77ef6da06f1c001c5adc6565402870
Rami Rantala <rami.rantala@zalando.fi>
2016-01-12 13:55:40 +0200

    small script for easier piu

---
+++ pau.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+select FN in $(senza list zmall-middleware -o tsv | grep -v stack_name | tr -s '\011' ' ' | cut -d" " -f1-2 | tr -s " " ":"); do
+	 echo $REPLY: $FN;
+	 stack_name=`echo "$FN" | cut -d":" -f1`
+	 stack_version=`echo "$FN" | cut -d":" -f2`
+	 echo $stack_name
+	 echo $stack_version
+	 instances=`senza instances $stack_name $stack_version -o tsv | grep -v stack_name | xargs | cut -d" " -f5`
+	 echo $instances
+	 cmd=`piu $instances "troubleshooting" | tail -1`
+
+	 $cmd
+	 exit 0
+done
